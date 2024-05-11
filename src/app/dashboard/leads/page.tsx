624ffa5d8206c85c5b29250192db6bcf7421d2b7
Soumik Pathak <soumikpathak0@gmail.com>
2024-05-11 18:18:29 +0530

    edit and upate lead form

--- src/app/dashboard/leads/page.tsx
+++ src/app/dashboard/leads/page.tsx
@@ -14,8 +14,8 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import React, { useState, useEffect } from 'react'
 import { app } from "../../firebaseConfig"
-import { getFirestore } from "firebase/firestore"
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import { deleteField, getFirestore } from "firebase/firestore"
+import { collection, addDoc, getDocs, doc, updateDoc } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'
 import {
     Table,
@@ -29,7 +29,10 @@ import {
 } from "@/components/ui/table"
 import { Pencil } from 'lucide-react'
 function page() {
-    const [leadData, setLeadData] = useState<Object[]>([])
+
+    const [leadData, setLeadData] = useState<Object[]>([]);
+    const [editForm, setEditForm] = useState({ name: '', number: '', business: '', id: '' })
+    const [open, setOpen] = useState(false);
     const auth = getAuth(app);
     const db = getFirestore(app);
     useEffect(() => {
@@ -59,6 +62,31 @@ function page() {
             console.error("Error adding document: ", e);
         }
     }
+    const editLead = (id: any, index: number, data: any) => {
+        setEditForm(data)
+        setOpen(true);
+    }
+
+    const submitEditedLead = async (e: any) => {
+        e.preventDefault()
+        console.log(editForm, editForm.id)
+        const userRef = doc(db, "leads", editForm.id);
+        await updateDoc(userRef, {
+            business: editForm.business,
+            name: editForm.name,
+            number: editForm.number,
+            // test: deleteField()
+        });
+        fetchLeadsData();
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { target } = event;
+        setEditForm((prevState) => ({
+            ...prevState,
+            [target.name]: target.value,
+        }));
+    };
     return (
         <div className='text-right px-10 py-4'>
             <Dialog >
@@ -68,7 +96,9 @@ function page() {
                 <DialogContent className="sm:max-w-[425px]">
                     <form onSubmit={createLead}>
                         <DialogHeader>
-                            <DialogTitle className='text-center'>Add New Lead</DialogTitle>
+                            <div className='py-4'>
+                                <DialogTitle className='text-center'>Add New Lead</DialogTitle>
+                            </div>
                             {/* <DialogDescription>
                             Make changes to your profile here. Click save when you're done.
                         </DialogDescription> */}
@@ -108,7 +138,7 @@ function page() {
                         </div>
                         <DialogFooter>
                             <DialogClose asChild>
-                                <Button type="submit" >Create Lead</Button>
+                                <Button type="submit" >Create</Button>
                             </DialogClose>
                         </DialogFooter>
                     </form>
@@ -131,7 +161,7 @@ function page() {
                                 <TableCell className="font-medium">{data.name}</TableCell>
                                 <TableCell className="font-medium">{data.number}</TableCell>
                                 <TableCell className="font-medium">{data.business}</TableCell>
-                                <TableCell><Pencil className="cursor-pointer" size={20} color="#2e70db" /></TableCell>
+                                <TableCell><Pencil className="cursor-pointer" size={20} color="#2e70db" onClick={() => editLead(data.id, index, data)} /></TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -139,7 +169,61 @@ function page() {
                 </Table>
             </div>
 
-
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent className="sm:max-w-[425px]">
+                    <form onSubmit={submitEditedLead}>
+                        <DialogHeader>
+                            <DialogTitle>Edit profile</DialogTitle>
+                            <DialogDescription>
+                                Make changes to your profile here. Click save when you're done.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label className="text-left">
+                                    Name
+                                </Label>
+                                <Input
+                                    type="text"
+                                    name="name"
+                                    className="col-span-3"
+                                    value={editForm.name}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label className="text-left">
+                                    Number
+                                </Label>
+                                <Input
+                                    type="number"
+                                    name="number"
+                                    className="col-span-3"
+                                    value={editForm.number}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                                <Label className="text-left">
+                                    Business Name
+                                </Label>
+                                <Input
+                                    type="text"
+                                    name="business"
+                                    className="col-span-3"
+                                    value={editForm.business}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <DialogClose asChild>
+                                <Button type="submit">Update</Button>
+                            </DialogClose>
+                        </DialogFooter>
+                    </form>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
